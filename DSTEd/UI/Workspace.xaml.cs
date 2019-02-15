be8ab67a1ebe713073df48a1ec8f7e341868d201
Adrian Preuß <adrian.preuss@icloud.com>
2019-02-15 22:25:32 +0100

    Start implementing Workspace selector

--- DSTEd/UI/Workspace.xaml.cs
+++ DSTEd/UI/Workspace.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace DSTEd.UI {
     public partial class Workspace : Window {
@@ -17,6 +18,27 @@ namespace DSTEd.UI {
 
         public void OnWindowClosing(object sender, CancelEventArgs e) {
             callback_close?.Invoke(e);
+        }
+
+        private void OnBrowse(object sender, RoutedEventArgs e) {
+            // @ToDo implement with own style (https://github.com/jkells/folder-browser-dialog-example/blob/master/FolderBrowserDialogEx.cs)
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            dialog.Title = "DST Location";
+            dialog.EnsureReadOnly = true;
+            dialog.IsFolderPicker = true;
+            dialog.AllowNonFileSystemItems = false;
+            dialog.Multiselect = false;
+            //dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            CommonFileDialogResult result = dialog.ShowDialog();
+
+            if (result == CommonFileDialogResult.Ok) {
+                this.path.Text = dialog.FileName;
+            }
+        }
+
+        private void OnSave(object sender, RoutedEventArgs e) {
+
         }
     }
 }
