14e8b47ff35e482bf16dad52bce0ba12691d43d7
Adrian Preuß <adrian.preuss@icloud.com>
2019-02-16 18:49:26 +0100

    Implement menu handling, change MessageBox to Dialog

---
+++ DSTEd/Core/Menu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Controls;
+using DSTEd.UI;
+
+namespace DSTEd.Core {
+    public class Menu {
+        private IDE ide = null;
+
+        public Menu(IDE ide) {
+            this.ide = ide;
+        }
+
+        public void Handle(string name, MenuItem menu) {
+            switch (name) {
+                //case "FILE_NEW_PROJECT":
+                //case "FILE_NEW_FILE":
+                //case "FILE_NEW_ASSET":
+                //case "FILE_OPEN":
+                //case "FILE_OPEN_RECENT":
+                //case "FILE_SAVE":
+                //case "FILE_SAVE_ALL":
+                //case "FILE_CLOSE":
+                //case "FILE_CLOSE_ALL":
+                case "FILE_EXIT":
+                    Dialog.Open("Do you want to exit DSTEd?", "Exit DSTEd", Dialog.Buttons.YesNo, Dialog.Icon.Warning, delegate (Dialog.Result result) {
+                        if (result == Dialog.Result.Yes) {
+                            Environment.Exit(0);
+                        }
+
+                        return true;
+                    });
+                    break;
+                //case "EDIT_UNDO":
+                //case "EDIT_REDO":
+                //case "EDIT_CUT":
+                //case "EDIT_COPY":
+                //case "EDIT_PASTE":
+                //case "EDIT_SELECT_ALL":
+                //case "SEARCH_FIND":
+                //case "SEARCH_FIND_NEXT":
+                //case "VIEW":
+                //case "DEBUG_RUN_DST":
+                //case "TOOLS_STEAM":
+                //case "TOOLS_SERVER":
+                //case "SETTINGS":
+                case "HELP_FORUM":
+                    Process.Start("https://forums.kleientertainment.com/forums/topic/78739-dsted-the-ide-for-dont-starve-together/");
+                    break;
+                //case "HELP_ABOUT":
+                case "HELP_FEEDBACK":
+                    Process.Start("https://github.com/DST-Tools/DSTEd-C/issues");
+                    break;
+                default:
+                    Logger.Warn("[Menu] Entry is not implemented: " + name);
+                    break;
+            }
+        }
+    }
+}
--- DSTEd/UI/IDE.xaml.cs
+++ DSTEd/UI/IDE.xaml.cs
@@ -1,32 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Media;
+﻿using System.Windows;
+using System.Windows.Forms;
 using DSTEd.Core;
 using DSTEd.UI.Theme;
-using MLib.Interfaces;
 using Xceed.Wpf.AvalonDock;
 using Xceed.Wpf.AvalonDock.Layout;
 
 namespace DSTEd.UI {
     public partial class IDE : Window {
+        private Core.Menu menu = null;
 
         public IDE() {
             InitializeComponent();
 
+            this.menu = new Core.Menu(this);
             this.dockManager.Theme = new Dark();
         }
 
         private void OnLayoutRootPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) {
             var activeContent = ((LayoutRoot) sender).ActiveContent;
 
-            if(e.PropertyName == "ActiveContent") {
+            if (e.PropertyName == "ActiveContent") {
                 Logger.Info(string.Format("ActiveContent-> {0}", activeContent));
             }
         }
 
         private void OnLoadLayout(object sender, RoutedEventArgs e) {
-            
+
         }
 
         private void OnSaveLayout(object sender, RoutedEventArgs e) {
@@ -34,25 +33,30 @@ namespace DSTEd.UI {
         }
 
         private void OnShowWinformsWindow(object sender, RoutedEventArgs e) {
-            
+
         }
 
         private void AddTwoDocuments_click(object sender, RoutedEventArgs e) {
-           
+
         }
 
         private void OnShowToolWindow1(object sender, RoutedEventArgs e) {
-           
+
         }
 
         private void dockManager_DocumentClosing(object sender, DocumentClosingEventArgs e) {
-            if (MessageBox.Show("Are you sure you want to close the document?", "DSTEd", MessageBoxButton.YesNo) == MessageBoxResult.No) {
+            Dialog.Open("Are you sure you want to close the document?", "DSTEd", Dialog.Buttons.YesNo, Dialog.Icon.Warning, delegate (Dialog.Result result) {
+                if (result == Dialog.Result.Yes) {
+                    return true;
+                }
+
                 e.Cancel = true;
-            }
+                return true;
+            });
         }
 
         private void OnDumpToConsole(object sender, RoutedEventArgs e) {
-           
+
         }
 
         private void OnReloadManager(object sender, RoutedEventArgs e) {
@@ -71,13 +75,26 @@ namespace DSTEd.UI {
         }
 
         private void OnToolWindow1Hiding(object sender, System.ComponentModel.CancelEventArgs e) {
-            if (MessageBox.Show("Are you sure you want to hide this tool?", "DSTEd", MessageBoxButton.YesNo) == MessageBoxResult.No) {
+            Dialog.Open("Are you sure you want to close this tool?", "DSTEd", Dialog.Buttons.YesNo, Dialog.Icon.Warning, delegate (Dialog.Result result) {
+                if (result == Dialog.Result.Yes) {
+                    return true;
+                }
+
                 e.Cancel = true;
-            }
+                return true;
+            });
         }
 
         private void OnShowHeader(object sender, RoutedEventArgs e) {
-            
+
+        }
+
+        private void OnMenu(object sender, RoutedEventArgs e) {
+            var item = sender as System.Windows.Controls.MenuItem;
+
+            if (item != null) {
+                this.menu.Handle(item.Name, item);
+            }
         }
     }
 }
