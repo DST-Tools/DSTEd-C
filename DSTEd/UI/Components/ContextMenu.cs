39515f78c0025494b7e3358437f4d045b7709895
Adrian Preuß <adrian.preuss@icloud.com>
2019-03-04 10:36:54 +0100

    Debugger: Adding hover & context menu for copy to clipboard

---
+++ DSTEd/UI/Components/ContextMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DSTEd.UI.Components {
+    public class ContextMenu : System.Windows.Controls.ContextMenu {
+        public void Add(string title, Action callback) {
+            MenuItem item = new MenuItem();
+            item.Header = title;
+            item.Click += new RoutedEventHandler(delegate (object sender, RoutedEventArgs e) {
+                callback?.Invoke();
+            });
+
+            this.AddChild(item);
+        }
+    }
+}
--- DSTEd/UI/Components/DebugEntry.cs
+++ DSTEd/UI/Components/DebugEntry.cs
@@ -1,10 +1,28 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using DSTEd.Core;
 
 namespace DSTEd.UI.Components {
     class DebugEntry : TextBlock {
+        private UI.Components.ContextMenu context = new UI.Components.ContextMenu();
+
         public DebugEntry(string content) {
             this.Text = content;
-            this.Padding = new System.Windows.Thickness(5, 2, 5, 2);
+            this.Padding = new Thickness(5, 2, 5, 2);
+            this.ContextMenu = context;
+
+            context.Add(I18N.__("Copy"), delegate () {
+                Clipboard.SetText(this.Text);
+            });
+
+            this.IsMouseDirectlyOverChanged += new DependencyPropertyChangedEventHandler(delegate (object sender, DependencyPropertyChangedEventArgs e) {
+                if (this.IsMouseOver) {
+                    this.Background = new SolidColorBrush(Color.FromArgb(100, 237, 92, 45));
+                } else {
+                    this.Background = new SolidColorBrush(Colors.Transparent);
+                }
+            });
         }
     }
 }
