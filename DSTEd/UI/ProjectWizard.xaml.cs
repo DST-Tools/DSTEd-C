954075dba814cdee0686184d7030feadd3bcc379
Akarinnnnn <1207247008@qq.com>
2019-04-02 01:18:55 +0800

    Project Wizard Need a UI

--- DSTEd/UI/ProjectWizard.xaml.cs
+++ DSTEd/UI/ProjectWizard.xaml.cs
@@ -22,47 +22,72 @@ namespace DSTEd.UI
 	{
 		//TODO: ProjectWizard UI
 		static Microsoft.VisualBasic.Devices.Computer computer = new Microsoft.VisualBasic.Devices.Computer();
+		static private DirectoryInfo[] TemplateList = new DirectoryInfo(".\\Project Templates").GetDirectories();
 		public ProjectWizard()
 		{
 			InitializeComponent();
 		}
 		/// <summary>
-		/// Overwrite existed files
+		/// Create a new Project from a specified template
 		/// </summary>
-		/// <param name="TemplateName">Which template to use</param>
+		/// <param name="Template">Use ParseTemplate() in this class</param>
 		/// <param name="TargetPath">Where to copy into</param>
-		/// <param name="ModName">Replace "__NAME" in filename\n"&lt;NAME%gt;"in Lua and SCML file</param>
-		public void CreateNewFromTemplate(string TemplateName, string TargetPath, string ModName)
+		public void CreateNewFromTemplate(DirectoryInfo Template, string TargetPath, string ModName)
 		{
-			var template = new DirectoryInfo("\\Project Templates\\" + TemplateName);
-			var target = Directory.CreateDirectory(Boot.Core().GetWorkspace().GetPath() + '\\' + TargetPath);
-			cp_x(template, target,ModName);
+			var target = Directory.CreateDirectory(TargetPath);
+			int count = cp_ren(Template, target, ModName);
+			Console.WriteLine("复制了{0}个文件", count);
+		}
+		/// <summary>
+		/// Parses Template Name into DirectoryInfo
+		/// </summary>
+		/// <returns>Returns a DirectoryInfo, From TemplateList</returns>
+		private DirectoryInfo ParseTemplate(string TemplateName)
+		{
+			foreach (DirectoryInfo template in TemplateList)
+				if (template.Name.ToLower() == TemplateName.ToLower())
+					return template;
+			throw new DirectoryNotFoundException("No such template");
 		}
 
-		private int cp_x(DirectoryInfo src,DirectoryInfo dest, string Replaces)//recursive copy!!
+		public void RefreshTemplateList()
+		{
+			TemplateList = new DirectoryInfo(".\\Project Templates").GetDirectories();
+		}
+		/// <summary>
+		/// Copy and replace specified filename/file content
+		/// </summary>
+		/// <param name="Replaces">Replace "__NAME" in filename or directory name,
+		/// and replace "&lt;NAME&gt;" in Lua and SCML file content</param>
+		/// <returns>Returns the count of copied file</returns>
+		private static int cp_ren(DirectoryInfo src, DirectoryInfo dest, string Replaces)//recursive copy!!
 		{
 			var srcfileref = src.GetFiles();
 			var srcdirref = src.GetDirectories();
 			int ret = srcfileref.Length;
 			foreach (FileInfo file in srcfileref)
 			{
-				var f2 = file.CopyTo(dest.FullName, true);
-				computer.FileSystem.RenameFile(f2.FullName, f2.FullName.Replace("__NAME", Replaces));
-				if(f2.Extension.ToLower() == ".lua" || f2.Extension.ToLower() == ".scml")
+				Directory.CreateDirectory(dest.FullName);
+				string newname = file.Name;
+				if (file.Name.Contains("__NAME"))
+					newname = file.Name.Replace("__NAME", Replaces);
+				var f2 = file.CopyTo(dest.FullName + '\\' + newname, true);
+				if (f2.Extension.ToLower() == ".lua" || f2.Extension.ToLower() == ".scml")
 				{
-					var fs = new FileStream(f2.FullName, FileMode.Open);
-					byte[] buffier = { 0 };
-					fs.Read(buffier, 0, (int)fs.Length);
-					fs = new FileStream(f2.FullName, FileMode.Truncate);
-					buffier = Encoding.UTF8.GetBytes(Encoding.UTF8.GetString(buffier).Replace("<NAME>", Replaces));
+					byte[] buffier = File.ReadAllBytes(f2.FullName);
+					FileStream fs = new FileStream(f2.FullName, FileMode.Truncate);
+					var replaceout = Encoding.UTF8.GetString(buffier).Replace(@"<NAME>", Replaces);
+					buffier = Encoding.UTF8.GetBytes(replaceout);
 					fs.Write(buffier, 0, buffier.Length);
 				}
 			}
 			foreach (DirectoryInfo directory in srcdirref)
 			{
-				ret += cp_x(directory, dest,Replaces);
-				string destname = dest.FullName + directory.Name;
-				computer.FileSystem.RenameDirectory(destname, directory.Name.Replace("__NAME", Replaces));
+				string destname = dest.FullName + '\\' + directory.Name;
+				var d2 = new DirectoryInfo(destname);
+				ret += cp_ren(directory, d2, Replaces);
+				if (destname.Contains("__NAME"))
+					computer.FileSystem.RenameDirectory(d2.FullName, directory.Name.Replace("__NAME", Replaces));
 			}
 			return ret;
 		}
