f4124d73d67d0a082e35aadc61384f716b3c6ead
Fa鸽 <1207247008@qq.com>
2020-02-11 01:23:46 +0800

    Add Core.IO.EnumerableFilesystem and it's test, details see XML Document

---
+++ DSTEd.Core.IO.FileSystem/FileSystemHelper.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DSTEd.Core.IO.EnumerableFileSystem
+{
+	/// <summary>
+	/// Gets all files(also in the subdirectories) in a directory to itertate
+	/// </summary>
+	public class RecursiveDirectoryIterator : IEnumerable<FileInfo>
+	{
+		List<FileInfo> internal_vector = new List<FileInfo>(50);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public DirectoryInfo OriginalDirectoryInfo { get; private set; }
+
+		/// <summary>
+		/// Get the number of founeded files
+		/// </summary>
+		public int Count => internal_vector.Count;
+		
+		/// <summary>
+		/// Initalize iterator by DirectoryInfo object.
+		/// </summary>
+		/// <param name="directory"></param>
+		public RecursiveDirectoryIterator(DirectoryInfo directory)
+		{
+			OriginalDirectoryInfo = directory;
+			RecursiveAdd(directory);
+		}
+
+		/// <summary>
+		/// Initalize iterator by path string.
+		/// </summary>
+		/// <param name="Path"></param>
+		public RecursiveDirectoryIterator(string Path):this(new DirectoryInfo(Path))
+		{
+
+		}
+
+		private void RecursiveAdd(DirectoryInfo dir)
+		{
+			internal_vector.AddRange(dir.EnumerateFiles());
+			System.Threading.Tasks.Parallel.ForEach(dir.EnumerateDirectories(), 
+				(DirectoryInfo file) => RecursiveAdd(file));
+		}
+
+		/// <summary>
+		/// Get the enumerator
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerator<FileInfo> GetEnumerator()
+		{
+			return internal_vector.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return internal_vector.GetEnumerator();
+		}
+
+		/// <summary>
+		/// </summary>
+		/// <param name="file"></param>
+		/// <returns>true for containd, false for not contained</returns>
+		public bool Contains(FileInfo file)
+		{
+			foreach (FileInfo source in internal_vector)
+			{
+				if (source.FullName == file.FullName)
+					return true;
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// indexer
+		/// </summary>
+		/// <param name="i"></param>
+		/// <returns></returns>
+		public FileInfo this[int i] => internal_vector[i];
+	}
+
+	/// <summary>
+	/// File system utilities
+	/// </summary>
+	public static class FSUtil
+	{
+		/// <summary>
+		/// Copy a directory into another directory,and keep the original directory structure
+		/// </summary>
+		/// <param name="Source">Source directory</param>
+		/// <param name="Destnation">Destnation directory</param>
+		/// <returns></returns>
+		public static RecursiveDirectoryIterator CopyDirectory(DirectoryInfo Source,DirectoryInfo Destnation)
+		{
+			//Destnation = Path.GetFileName(Destnation);
+			return CopyDirectory(new RecursiveDirectoryIterator(Source), Destnation);
+		}
+
+		/// <summary>
+		/// Copy all specified files into a new directory
+		/// </summary>
+		/// <param name="Iterator">Source directory</param>
+		/// <param name="TargetDirectory"></param>
+		/// <returns></returns>
+		public static RecursiveDirectoryIterator CopyDirectory(RecursiveDirectoryIterator Iterator,DirectoryInfo TargetDirectory)
+		{
+			return CopyFilesToDirectory(Iterator, Iterator.OriginalDirectoryInfo, TargetDirectory);
+		}
+
+		/// <summary>
+		/// Copy all specified files into a new directory
+		/// </summary>
+		/// <param name="Files"></param>
+		/// <param name="SourceDirectory"></param>
+		/// <param name="TargetDirectory"></param>
+		/// <returns></returns>
+		public static RecursiveDirectoryIterator CopyFilesToDirectory(IEnumerable<FileInfo> Files, DirectoryInfo SourceDirectory , DirectoryInfo TargetDirectory)
+		{
+			foreach (FileInfo file in Files)
+			{
+				string filedest = TargetDirectory.FullName + '\\' + SimpleRelative(SourceDirectory.FullName, file.FullName);
+				try
+				{
+					Directory.CreateDirectory(System.IO.Path.GetDirectoryName(filedest));
+					file.CopyTo(filedest);
+				}
+				catch (DirectoryNotFoundException e)
+				{
+					System.Diagnostics.Debug.WriteLine("Direcotry not found?\n" +
+						filedest + '\n' +
+						"HRESULT:\n" +
+						e.HResult);
+					Directory.CreateDirectory(System.IO.Path.GetDirectoryName(filedest));
+					file.CopyTo(filedest);
+				}
+				catch (FileNotFoundException e)
+				{
+					Console.WriteLine(
+						"????????BUG???????\n" +
+						"Check FSUtil.RecursiveDirectoryItertatior\n" +
+						"Stack Traceback:\n{1}\n" +
+						"Message:\n{2}\n" +
+						"HRESULT:\n{3}\n",
+						e.StackTrace, e.Message, e.HResult);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.ToString() + e.HResult);
+				}
+#if DEBUG
+				System.Diagnostics.Debug.WriteLine("Copy {0} to {1}", file.FullName, filedest);
+#endif
+			}
+			return new RecursiveDirectoryIterator(TargetDirectory);
+		}
+
+		/// <summary>
+		/// Get relative path from two full path strings.
+		/// </summary>
+		/// <param name="Parent">parent directory</param>
+		/// <param name="File">file</param>
+		/// <returns></returns>
+		public static string SimpleRelative(string Parent, string File)
+		{
+			return File.Replace(Parent, string.Empty);
+		}
+
+		/// <summary>
+		/// Filter out some specified file in a FileInfo collection
+		/// </summary>
+		/// <param name="Files"></param>
+		/// <param name="Extensions">like ".jpg",".lua"</param>
+		/// <returns>A List{FileInfo} contains the filtered out files</returns>
+		/// <example>ApplyFilter(files,".jpg",".png",".lua")</example>
+		public static List<FileInfo> ApplyFilter(ICollection<FileInfo> Files,params string[] Extensions)
+		{
+			List<FileInfo> ret_value = new List<FileInfo>(Files.Count);
+			foreach (FileInfo file in Files)
+			{
+				foreach (string ext in Extensions)
+				{
+					if (file.Extension == ext)
+						ret_value.Add(file);
+				}
+			}
+			return ret_value;
+		}
+	}
+}
---
+++ DSTEd.Core.IO.FileSystem/path.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DSTEd.Core.IO.EnumerableFileSystem
+{
+	/// <summary>
+	/// Repersents a Windows path
+	/// </summary>
+	[Serializable]
+	public class Path :ISerializable
+	{
+		//private members
+		private string original;
+
+		//props
+
+		/// <summary>
+		/// Gets the extension.
+		/// </summary>
+		/// <remarks>a file named like ".hidden" will treated as no extension exists</remarks>
+		public string Extension { get; private set; }
+
+		/// <summary>Get Filename, this property will be string.Empty in case of a path like "/p/"</summary>
+		/// <remarks></remarks>
+		public string FileName { get; private set; }
+
+		/// <summary>
+		/// Get parent path
+		/// </summary>
+		public string Parent { get; private set; }
+
+		//ctor
+		/// <summary>
+		/// Initalize an instance of path class by a source path string
+		/// </summary>
+		/// <param name="source">Path string</param>
+		/// <exception cref="ArgumentException">source is an empty string or null</exception>
+		public Path(string source)
+		{
+			Initialize(source);
+		}
+
+		private void Initialize(string source)
+		{
+			if (source == null || source == string.Empty)
+				throw new ArgumentException(source, nameof(source));
+			original = source.Replace('/', '\\');
+			string[] parts = original.Split('\\');
+			FileName = parts.Last();
+			//extension
+			{
+				int pt_index = FileName.Length - 1;
+				for (; pt_index > 0; pt_index--)
+				{
+					if (FileName[pt_index] == '.')
+						break;
+				}
+				Extension = FileName.Substring(pt_index);
+			}
+		}
+
+		/// <summary>
+		/// Get relative path
+		/// </summary>
+		/// <param name="L"></param>
+		/// <param name="R"></param>
+		/// <returns></returns>
+		public static Path RelativePath(Path L, Path R)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Get absulote
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="child"></param>
+		/// <returns></returns>
+		public static Path Canonical(Path parent, Path child)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Append another path to this path. 
+		/// </summary>
+		/// <param name="r">Another path</param>
+		/// <returns></returns>
+		public Path Append(Path r)
+		{
+			original = Regex.Replace(original + r.original, "\\+", "\\");
+			return this;
+		}
+
+#pragma warning disable CS1591 // 缺少对公共可见类型或成员的 XML 注释
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+#pragma warning restore CS1591 // 缺少对公共可见类型或成员的 XML 注释
+		{
+			info.AddValue("original", original);
+		}
+
+		/// <summary>
+		/// Appends Right path to Left Path
+		/// </summary>
+		/// <param name="l">Left</param>
+		/// <param name="r">Right</param>
+		/// <returns></returns>
+		public static Path operator /(Path l, Path r)
+		{
+			string ret_value = Regex.Replace(l.original + r.original, "\\+", "\\");
+			return new Path(ret_value);
+		}
+
+		//private methods
+
+	}
+}
