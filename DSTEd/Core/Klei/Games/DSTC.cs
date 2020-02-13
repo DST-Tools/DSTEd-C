a64ee11b9f3179c6083bbfca127fb93cccd2ed77
Adrian Preuß <adrian.preuss@icloud.com>
2020-02-13 16:30:20 +0100

    Adding addotional informations

--- DSTEd/Core/Klei/Games/DSTC.cs
+++ DSTEd/Core/Klei/Games/DSTC.cs
@@ -7,7 +7,18 @@
             this.SetMainGame();
 
             //this.AddDebug("RUN_DST", this.executable);
-			//not supported so far. [Akarinnnnn]:try using another process to run?
+            //not supported so far. [Akarinnnnn]:try using another process to run?
+            /*
+                comment by Bizarrus:
+
+                The problem is in the execution.
+                Here's what we want to do with the client:
+                    - the client should be started by DSTEd
+                    - All output and error streams should run through DSTEd
+                    - The execution must not be started in a separate, encapsulated process,
+                      but must be managed by the main application DSTEd. Otherwise, you cannot
+                      attach yourself to the process.
+             */
         }
     }
 }
